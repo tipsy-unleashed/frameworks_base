e25a8aae879d297d0982f74e071545ddfd659f06
Diogo Ferreira <defer@cyngn.com>
2014-12-01 17:17:21 +0000

    Recents: Port "Development shortcut" (2/2)
    This patch includes the equivalent of our Development Shortcuts in
    CM11 recents. Recents is very different now and this is mostly a rewrite.
    
    The semantics changed a bit as well. The current code already has special
    developer behavior when long-pressing the app icon and that is also where
    the developer shortcuts are hooked. If the app icon is long-pressed and
    developer shortcuts are active, a menu will be shown with the several available
    options.
    
    Fixes CML-61
    
    Change-Id: I391a360b3dd88e179c2315503d18125f8d0b3792
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6518,6 +6518,14 @@ public final class Settings {
         public static final String APP_PERFORMANCE_PROFILES_ENABLED = "app_perf_profiles_enabled";
 
         /**
+         * Whether to display a menu containing 'Wipe data', 'Force close' and other options
+         * in the notification area and in the recent app list
+         * @hide
+         */
+        public static final String DEVELOPMENT_SHORTCUT = "development_shortcut";
+
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -6572,7 +6580,8 @@ public final class Settings {
             PRIVACY_GUARD_NOTIFICATION,
             ADVANCED_REBOOT,
             STATUS_BAR_LOCKED_ON_SECURE_KEYGUARD,
-            LOCKSCREEN_QUICK_UNLOCK_CONTROL
+            LOCKSCREEN_QUICK_UNLOCK_CONTROL,
+            DEVELOPMENT_SHORTCUT
         };
 
         /**
