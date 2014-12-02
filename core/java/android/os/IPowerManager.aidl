b79965c4ecf7cd3c54d32d1c9159efca279eda41
faust93 <monumentum@gmail.com>
2014-12-02 12:26:57 +0300

    Wakelock Blocker [1/2]
    Change-Id: I658e8ad54dc34735e920cf2e942cab15aa3eaa2e
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/os/IPowerManager.aidl
+++ core/java/android/os/IPowerManager.aidl
@@ -69,4 +69,7 @@ interface IPowerManager
     String getPowerProfile();
 
     void activityResumed(String componentName);
+
+    // blocked wakelock support
+    String getSeenWakeLocks();
 }
--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3569,6 +3569,18 @@ public final class Settings {
         public static final String PIE_SIZE = "pie_size";
 
          /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Whether to dismiss all recents tasks
          * @hide
          */
