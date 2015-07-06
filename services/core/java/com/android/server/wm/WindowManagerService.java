c96a7e2f4a47d8e02d65695b938dc389f17c7487
TheBr0ken <jtoro2716@gmail.com>
2015-07-06 15:49:33 -0500

    Tweak animations a bit
    x0.75 > x0.5
    
    Change-Id: I377b2d69e30beedfcd2d2b3b805fe45859118692
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	services/core/java/com/android/server/wm/WindowManagerService.java

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -634,9 +634,9 @@ public class WindowManagerService extends IWindowManager.Stub
     PowerManager mPowerManager;
     PowerManagerInternal mPowerManagerInternal;
 
-    float mWindowAnimationScaleSetting = 0.60f;
-    float mTransitionAnimationScaleSetting = 0.60f;
-    float mAnimatorDurationScaleSetting = 0.60f;
+    float mWindowAnimationScaleSetting = 0.5f;
+    float mTransitionAnimationScaleSetting = 0.5f;
+    float mAnimatorDurationScaleSetting = 0.5f;
     boolean mAnimationsDisabled = false;
 
     final InputManagerService mInputManager;
