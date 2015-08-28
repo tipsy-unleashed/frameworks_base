63eefeca6810bd48ddd05e4fc309957cc7ebca10
LordClockaN <davor@losing.com>
2015-08-28 14:11:55 -0600

    FWB: Toast icon switch [1/2]
    Change-Id: I548ee8ffd8a746962556b062949c46a6eefc3bf1
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	core/java/android/widget/Toast.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4282,6 +4282,13 @@ public final class Settings {
         public static final String LOCKSCREEN_CLOCK_DATE_COLOR = "lockscreen_clock_date_color";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
