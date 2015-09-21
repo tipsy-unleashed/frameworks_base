a39ca5040a2f8c5e64021656a9233c265dcc643e
Janson Kang <temasek71@gmail.com>
2015-09-21 09:57:50 +0800

    SystemUI: Add option for new Poly HD custom contextual headers (1/2)
    Change-Id: I91eb2b2aa2f43e896bf17270b92e58aa3fea958c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4333,6 +4333,14 @@ public final class Settings {
         public static final String WHITELIST_APP_CIRCLE_BAR = "whitelist_app_circle_bar";
 
         /**
+         * Whether to use default custom header or poly hd
+         * 0 = default
+         * 1 = poly hd
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_DEFAULT = "status_bar_custom_header_default";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
