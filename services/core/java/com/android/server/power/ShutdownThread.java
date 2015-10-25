42895fbf99964b41bb936470d4bb407cbd72b3fc
Alex Cruz <mazdarider23@gmail.com>
2015-10-25 00:15:28 +0000

    Turn advanced reboot on by default [1/2]
    Change-Id: Iac9a74bc70f16a2d191589fa55542441fa1f5312
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	services/core/java/com/android/server/power/ShutdownThread.java

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -262,7 +262,7 @@ public final class ShutdownThread extends Thread {
     private static int getAdvancedReboot(Context context) {
         int def = BuildInfo.getSlimBuildType().equals(BuildInfo.BUILD_TYPE_UNOFFICIAL) ? 1 : 0;
         return Settings.Secure.getInt(context.getContentResolver(),
-                Settings.Secure.ADVANCED_REBOOT, def);
+                Settings.Secure.ADVANCED_REBOOT, 1);
     }
 
     private static void doSoftReboot() {
