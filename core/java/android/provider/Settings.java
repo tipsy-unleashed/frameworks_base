a80be98a3fd6523221f5003fd1bac59fd959a176
cristianomatos <cristianobmatos@gmail.com>
2015-11-15 20:21:36 -0500

    Recents panel clear all button tweaks.
    - Show or hide recents button
    - Place the button in top right or left
    
    Change-Id: I3a0f8049648407c30f90265cf699ade7b1cbac04
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3807,6 +3807,18 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * show clear all recents button
+         *  @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Action to perform when the app switch key is double tapped. (default: ACTION_NULL)
          * (See ButtonsConstants.java for valid values)
          * @hide
