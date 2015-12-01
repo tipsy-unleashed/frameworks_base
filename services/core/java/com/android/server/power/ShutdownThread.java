ba51426761ed0208fce94072dce1b25f73c10963
xWASABI <wasabi.dev@gmail.com>
2015-12-01 10:15:56 -0500

    Frameworks/Systemui DayNight update
    Global Actions:
    ** Preliminary Global Acions DayNight initial commit picked from John Brewer.
    ** https://github.com/TeslaProject-Marshmallow/android_frameworks_base/commit/659c732950db56f5e3d2050c9067ef9fb7f01b82
    
    ** Updated a few lines in code for better support with power menu icons and
    ** text colors.
    
    SystemUI - Initial DayNight commit:
    ** Added support for theming notification header and quick settings panel by
    ** changing systemui_theme and systemui_theme_light parent.
    
    ** Added essential colors to values-night/ to have it theme when night is
    ** enabled.
    
    ** Added the needed colors into frameworks core for theming notification text
    ** colors when night is enabled.
    
    Note:
    ** Restarting SystemUI or rebooting phone is needed for "night" theme to apply
    ** for notification header, quick settings panel, &  notification backgrounds.
    
    ** Added color values for TeamTwisted "night" theme bringup.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -198,7 +198,7 @@ public final class ShutdownThread extends Thread {
 
                 if ((advancedReboot == 1 && !locked) || advancedReboot == 2) {
                     // Include options in power menu for rebooting into recovery or bootloader
-                    sConfirmDialog = new AlertDialog.Builder(context)
+                    sConfirmDialog = new AlertDialog.Builder(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert)
                             .setTitle(titleResourceId)
                             .setItems(
                                     com.android.internal.R.array.shutdown_reboot_options,
@@ -238,7 +238,7 @@ public final class ShutdownThread extends Thread {
             }
 
             if (sConfirmDialog == null) {
-                sConfirmDialog = new AlertDialog.Builder(context)
+                sConfirmDialog = new AlertDialog.Builder(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert)
                         .setTitle(titleResourceId)
                         .setMessage(resourceId)
                         .setPositiveButton(com.android.internal.R.string.yes,
@@ -345,7 +345,7 @@ public final class ShutdownThread extends Thread {
         }
 
         // Throw up a system dialog to indicate the device is rebooting / shutting down.
-        ProgressDialog pd = new ProgressDialog(context);
+        ProgressDialog pd = new ProgressDialog(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert);
 
         // Path 1: Reboot to recovery and install the update
         //   Condition: mRebootReason == REBOOT_RECOVERY and mRebootUpdate == True
@@ -848,3 +848,4 @@ public final class ShutdownThread extends Thread {
         }
     }
 }
+
