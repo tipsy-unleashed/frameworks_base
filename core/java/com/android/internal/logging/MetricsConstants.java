a2b2194cd88cdf80944ce4ee4255db2aa76d6abb
John Brewer <gzroms@hotmail.com>
2015-12-05 12:24:24 -0500

    oops

--- core/java/com/android/internal/logging/MetricsConstants.java
+++ core/java/com/android/internal/logging/MetricsConstants.java
@@ -265,7 +265,7 @@ public interface MetricsConstants {
     public static final int FUELGAUGE_INACTIVE_APPS = 238;
 
     // Tesla 
-    public static final int TESLA_ABOUT = 239
+    public static final int TESLA_ABOUT = 239;
 
     // These constants must match those in the analytic pipeline, do not edit.
     // Add temporary values to the top of MetricsLogger instead.
