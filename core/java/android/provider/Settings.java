bb83e123321f2de0d11b5a592e6214597b0ec487
kufikugel <kufikugel@googlemail.com>
2015-12-14 10:04:46 -0500

    Base: Navigation bar options (1/2) -Toggle on/off -Height and width

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4134,6 +4134,30 @@ public final class Settings {
         public static final String GESTURE_ANYWHERE_SHOW_TRIGGER = "gesture_anywhere_show_trigger";
 
         /**
+         * Navigation bar height when it is on protrait
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+        /**
+         * Navigation bar height when it is on landscape
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
+
+        /**
+         * Navigation bar height when it is on landscape at the right
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
+
+        /**
+         * Wether navigation bar is enabled or not
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
+
+        /**
          * Whether to enable status and navigation bar color in battery saver mode.
          *
          * @hide
