c3420d74ae12703eb2671fefca1fac19995174f6
theringmaster <gzroms@hotmail.com>
2016-01-02 20:07:19 -0500

    Build fixes

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowManager.java
@@ -50,6 +50,7 @@ public class StatusBarWindowManager {
     private WindowManager.LayoutParams mLpChanged;
     private int mBarHeight;
     private boolean mKeyguardScreenRotation;
+    private final float mScreenBrightnessDoze;
     private final State mCurrentState = new State();
 
     public StatusBarWindowManager(Context context) {
