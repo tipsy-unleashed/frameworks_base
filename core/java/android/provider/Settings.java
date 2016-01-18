29b3448ea71585557d4bd71c3a3d68fd13a7596e
LorDClockaN <davor@losinj.com>
2016-01-18 12:32:54 -0500

    FWB: QS shade transparency (1/2)
    Change-Id: Ib69cea4ba01119ca75a93afe0c08a9479497342c
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4606,6 +4606,12 @@ public final class Settings {
          */
         public static final String DASHBOARD_FONT_STYLE = "dashboard_font_style";
         /**
+         * Transparent qs shade
+         * @hide
+         */
+        public static final String QS_TRANSPARENT_SHADE = "qs_transparent_shade";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
