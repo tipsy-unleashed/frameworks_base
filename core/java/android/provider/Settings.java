a0ead7d0402394d0ba5b3a38533465122ed9f155
Varun Date <date.varun123@gmail.com>
2016-04-22 09:51:43 +0530

    Keyguard Charging Current Switch [1/2]
    Based on this Patch
    
    https://github.com/AICP/frameworks_base/commit/a86888febb62520de117ad6a01740d651ac96044
    
    Thanks to yank555-lu for the initial patch
    
    Just adding an option to turn it on/off
    
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4518,6 +4518,12 @@ public final class Settings {
         public static final String SETTINGS_CATEGORY_TEXT_SIZE = "settings_category_text_size";
 
         /**
+         * whether to enable /disable showing charging current on lockscreen
+         * @hide
+         */
+        public static final String LOCK_SCREEN_SHOW_CURRENT = "lock_screen_show_current";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
